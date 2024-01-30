c44e9c50833930c0ba25dc97149db6ba61318295
omega2 <rin-s@omega-inc.co>
2024-01-30 16:58:42 +0900

    add more routes for placeholder

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-# The below lines are not needed for production in App Engine
+@app.route('/terms-of-use')
+def terms_of_use():
+    return 'Terms of Use'
+
+@app.route('/privacy-policy')
+def privacy_policy():
+    return 'Privacy Policy'
+
+# The below lines are not used for production in App Engine
 if __name__ == '__main__':
-     app.run(debug=False)
+     app.run(host='127.0.0.1', port=8080, debug=True)
